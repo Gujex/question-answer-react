cfb2b263e1ee8779f0079ca89876b339258a9de7
Gujex <txere@bk.ru>
2021-11-13 22:41:13 +0400

    Updates

---
+++ precache-manifest.20c1f8f048318ef8ee97edb07a3221e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e405bebd9830b93e5a9c1c3f1763802",
+    "url": "/question-answer-react/index.html"
+  },
+  {
+    "revision": "66bcd1337289bf0ebddb",
+    "url": "/question-answer-react/static/css/main.3d640bf4.chunk.css"
+  },
+  {
+    "revision": "ab85a117f39d501e282e",
+    "url": "/question-answer-react/static/js/2.40b1d22a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/question-answer-react/static/js/2.40b1d22a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66bcd1337289bf0ebddb",
+    "url": "/question-answer-react/static/js/main.9ea25907.chunk.js"
+  },
+  {
+    "revision": "fb3e2d7e0893ae00ac6e",
+    "url": "/question-answer-react/static/js/runtime-main.25c381e8.js"
+  }
+]);
